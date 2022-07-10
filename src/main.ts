60a2e5170acac9315a9334bb52ab1a115fc954a6
nouvist <hello@nouvist.cyou>
2022-07-10 17:47:34 +0700

    feat: FunctionDeclaration with identifier implementation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,70 @@
-import { PluginObj } from '@babel/core';
+import { PluginObj, transformFromAstSync, types } from '@babel/core';
 
 export default function babelPluginShowSource(): Partial<PluginObj> {
   return {
     visitor: {
-      Function(path) {
-        console.log(path);
+      Directive(path) {
+        const isShowSource = path.node.value.value === 'show source';
+        if (!isShowSource) return;
+
+        const func = path.parentPath.parentPath;
+        if (func === null || !func.isFunctionDeclaration()) return;
+
+        const funcParent = func.parentPath;
+
+        if (
+          funcParent === null ||
+          !(funcParent.isBlockStatement() || funcParent.isProgram())
+        )
+          return;
+
+        if (!func.node.id?.name) console.warn('not implemented');
+
+        const funcIndex = funcParent.node.body.indexOf(func.node);
+
+        const funcCode = transformFromAstSync(
+          types.program([func.node]),
+          undefined,
+          {
+            filename: 'unknown.js',
+          },
+        )?.code;
+
+        const definePropertyStatement = types.callExpression(
+          types.memberExpression(
+            types.identifier('Object'),
+            types.identifier('defineProperty'),
+          ),
+          [
+            types.identifier(func.node.id!.name),
+            types.objectExpression([
+              types.objectMethod(
+                'method',
+                types.identifier('toString'),
+                [],
+                types.blockStatement([
+                  types.returnStatement(
+                    types.stringLiteral(
+                      funcCode ||
+                        'function () { throw new Error("Function.prototype.toString() not generated correctly"); }',
+                    ),
+                  ),
+                ]),
+              ),
+            ]),
+          ],
+        );
+
+        const newBody: types.Statement[] = [
+          ...funcParent.node.body.slice(0, funcIndex + 1),
+          definePropertyStatement,
+          ...funcParent.node.body.slice(funcIndex + 1),
+        ];
+
+        funcParent.node.body = newBody;
       },
     },
   };
 }
+
+function is() {}
