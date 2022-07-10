85cf654258ac7c562dd07a8b806b51e19ba5d9eb
nouvist <hello@nouvist.cyou>
2022-07-10 20:32:07 +0700

    feat: use replaceWithMultiple instead do it manually

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,45 @@
 import { PluginObj, types } from '@babel/core';
 import createInjectionStatement from './createInjectionStatement';
+import createInjectionWrapper from './createInjectionWrapper';
 import { functionDeclarationToString } from './functionDeclarationToString';
 
 interface Options {
-  removeDirective?: boolean;
+  removeDirective: boolean;
 }
 
 export default function babelPluginShowSource(
-  options?: Options,
+  _options?: Partial<Options>,
 ): Partial<PluginObj> {
+  const options: Options = {
+    removeDirective: false,
+    ..._options,
+  };
+
   return {
     visitor: {
       Directive(path) {
         const isShowSource = path.node.value.value === 'show source';
         if (!isShowSource) return;
-
-        const func = path.parentPath.parentPath;
         if (options.removeDirective) path.remove();
 
-        if (func === null || !func.isFunctionDeclaration()) return true;
+        const func = path.parentPath.parentPath;
+        if (func === null) return;
 
         const funcParent = func.parentPath;
 
         if (
-          funcParent === null ||
-          !(funcParent.isBlockStatement() || funcParent.isProgram())
-        )
-          return true;
+          func.isFunctionDeclaration() &&
+          funcParent !== null &&
+          (funcParent.isBlockStatement() || funcParent.isProgram())
+        ) {
+          const funcCode = functionDeclarationToString(func.node);
+          const injectionStatement = createInjectionStatement(
+            func.node.id!.name,
+            funcCode,
+          );
 
-        if (!func.node.id?.name) console.warn('not implemented');
-
-        const funcIndex = funcParent.node.body.indexOf(func.node);
-
-        const funcCode = functionDeclarationToString(func.node);
-        const injectionStatement = createInjectionStatement(
-          func.node.id!.name,
-          funcCode,
-        );
-
-        const newBody: types.Statement[] = [
-          ...funcParent.node.body.slice(0, funcIndex + 1),
-          injectionStatement,
-          ...funcParent.node.body.slice(funcIndex + 1),
-        ];
-
-        funcParent.node.body = newBody;
+          func.replaceWithMultiple([func.node, injectionStatement]);
+        }
       },
     },
   };
