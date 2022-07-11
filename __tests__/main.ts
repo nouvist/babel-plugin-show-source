27ae842d342f2c25a725a1af23f41c790a520e99
nouvist <hello@nouvist.cyou>
2022-07-11 14:10:17 +0700

    chore: use jest for testing

---
+++ __tests__/main.ts
@@ -0,0 +1,76 @@
+import { transformSync } from '@babel/core';
+import { Script } from 'vm';
+import babelPluginShowSource from '../src/main';
+
+function run(_code: string, transform?: boolean) {
+  let code: string;
+  if (transform)
+    code =
+      transformSync(_code, {
+        filename: 'unknown.ts',
+        plugins: [
+          babelPluginShowSource({
+            removeDirective: true,
+          }),
+        ],
+      })?.code || '';
+  else code = _code;
+
+  const injected = `
+  // simulate no Function.prototype.toString();
+  Function.prototype.toString = () => null;
+  function str(func) {
+    return func.toString();
+  }
+  ${code}`;
+
+  const script = new Script(injected).runInNewContext();
+
+  try {
+    return new Script(`(${script})();`).runInNewContext();
+  } catch {
+    return null;
+  }
+}
+
+test('FunctionDeclaration', () => {
+  const num = Math.round(Math.random() * 1e4);
+
+  const code = `
+  function foobar() {
+    'show source';
+    return ${num};
+  }
+  str(foobar);`;
+
+  expect(run(code)).toBe(null);
+  expect(run(code, true)).toBe(num);
+});
+
+test('FunctionExpression', () => {
+  const num = Math.round(Math.random() * 1e4);
+
+  const code = `
+  const foobar = function() {
+    'show source';
+    return ${num};
+  };
+  str(foobar);`;
+
+  expect(run(code)).toBe(null);
+  expect(run(code, true)).toBe(num);
+});
+
+test('ArrowFunction', () => {
+  const num = Math.round(Math.random() * 1e4);
+
+  const code = `
+  const foobar = () => {
+    'show source';
+    return ${num};
+  };
+  str(foobar);`;
+
+  expect(run(code)).toBe(null);
+  expect(run(code, true)).toBe(num);
+});
